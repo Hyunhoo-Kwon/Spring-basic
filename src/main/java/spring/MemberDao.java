7ba27980a7042c5e18799ed6580693159fba5d6c
HyunhooKwon <kwongusgn@gmail.com>
2017-11-12 13:42:15 +0900

    MemberDao.java 클래스 구현

--- src/main/java/spring/MemberDao.java
+++ src/main/java/spring/MemberDao.java
@@ -1,8 +1,13 @@
 package spring;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class MemberDao {
 	
@@ -10,6 +15,26 @@ public class MemberDao {
 	
 	public MemberDao(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	public Member selectByEmail(String email) {
+		List<Member> results = jdbcTemplate.query(
+				"SELECT * FROM MEMBER WHERE EMAIL = ?", 
+				new RowMapper<Member>() {
+					@Override
+					public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+						Member member = new Member(rs.getString("EMAIL"),
+								rs.getString("PASSWORD"),
+								rs.getString("NAME"),
+								rs.getTimestamp("REGDATE")
+								);
+						member.setId(rs.getLong("ID"));
+						return member;
+					}
+				}
+				,email);
+		
+		return results.isEmpty() ? null : results.get(0);
 	}
 
 }
